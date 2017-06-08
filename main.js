4e79df6861540934f8ec2fb5e9970d34234b9c37
earthmaiden <earthmaiden@users.noreply.github.com>
2017-06-07 23:08:57 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+
+
+//$('#buyNowButton').click(function() {
+//    console.log('hey');
+//  $('#addedItem').toggle();
+//});
+
+$('#buyNowButton').click(function(){
+  console.log('test')
+  $('#buyNowButton').appendto('buyNowButton')
+});
+
+//console.log("lol trying to come up with ideas")
+
+//var trigger = document.getElementById("buyNowButton")
+//var modal = document.getElementById("addedItem")
+//var close = document.getElementById("closeBtn")
+
+//trigger.addEventListener('click', function addItem(){
+//  console.log("buyNowButton clicked")
+//  modal.style.visibility="visible";
+//});
+
+//close.addEventListener('click', function addItem(){
+  //console.log("buyNowButton clicked")
+  //modal.style.visibility="hidden";
+//});
+
+//function addItem(){
+  //onclick = true;
+  //document.getElementById("buyNowButton")
+//}
+//
+
+//$("#buyNowButton").click(function() {
+//  $("#addedItem").css("visibility" : "visible");
+//});
+
+
+
+//$("#buyNowButton").click( function())
+
+//$(".addedItem").css("background-color", "yellow")
